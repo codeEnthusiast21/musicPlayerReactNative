77da87881be0117cd4f9051eb2365c43a5fa9e06
codeEnthusiast21 <aviralagarwal190006@gmail.com>
2025-06-08 00:25:24 +0530

    structuring

---
+++ src/screens/MusicPlayer.tsx
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#001d23',
+    },
+    listArtWrapper: {
+      width: width,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    albumContainer: {
+      width: 300,
+      height: 300,
+    },
+    albumArtImg: {
+      height: '100%',
+      borderRadius: 4,
+    },
+  });
